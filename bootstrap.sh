fc402462f3f574ea202a95828118e4f55f02b67e
Lex Harris <heartscanner@gmail.com>
2022-02-25 21:20:46 -0500

    Initial Commit

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+###########################################################################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox                            #
+# Updated by One Home for crypto use...                                                                #
+# This script is intended to be run like this:                                                            #
+#                                                                                                         #
+#  curl https://raw.githubusercontent.com/onehomedev/Yiimp-Server-Installer/master/bootstrap.sh | bash # 
+#                                                                                                         #
+###########################################################################################################
+
+
+# Clone the Yiimp Server repository if it doesn't exist.
+
+echo Installing git . . .
+apt-get -q -q update
+apt-get -q -q install -y git < /dev/null
+echo
+
+echo Downloading Yiimp Server Installer v1.0. . .
+git clone https://github.com/onehomedev/yiimp_server_setup "$HOME"/yiimpserver/install < /dev/null 2> /dev/null
+echo
+
+# Start setup script.
+bash $HOME/yiimpserver/install/start.sh
